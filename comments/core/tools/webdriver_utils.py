915ffce166c05eeb4982c7bfe0a357de4739f9df
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2017-07-26 17:56:37 +0300

    Add WebdriverUtils
    WebdriverUtils class is designed to open/close specific browser and open URLs.
    
    Affects #5

---
+++ comments/core/tools/webdriver_utils.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+
+
+class WebdriverUtils(object):
+    def __init__(self, driver_name: str):
+        self._driver_name = driver_name.lower()
+        self._driver = None
+
+    def get_driver(self):
+        if self._driver is None:
+            if self._driver_name == 'chrome':
+                capabilities = DesiredCapabilities.CHROME.copy()
+                port = 9515
+            elif self._driver_name == 'firefox':
+                capabilities = DesiredCapabilities.FIREFOX.copy()
+                port = 4444
+            else:
+                raise Exception("Browser is not supported")
+            try:
+                self._driver = webdriver.Remote(port, capabilities)
+            except Exception:
+                raise Exception("Can't initialize webdriver")
+            self._driver.implicitly_wait(10)
+            self._driver.maximize_window()
+        return self._driver
+
+    def load(self, url: str):
+        self.get_driver().get(url)
+
+    def refresh(self):
+        self.get_driver().refresh()
+
+    def stop(self):
+        if self._driver is not None:
+            self._driver.delete_all_cookies()
+            self._driver.close()
+        self._driver = None
