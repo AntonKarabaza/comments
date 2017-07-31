41f63eeb1d5490f8f4d3e2747a93d01fc2349b0e
AntonKarabaza <antonkarabaza@google.com>
2017-07-31 14:20:08 +0300

    Add Driver interface and RemoteDriver class
    In module webdriver_utils.py add interface Driver and class
    RemoteDriver implementing it.
    Remove class WebDriverUtils since its functionality was moved to
    RemoteDriver and WebDriverFactory classes.
    
    Affects #11

--- comments/core/tools/webdriver_utils.py
+++ comments/core/tools/webdriver_utils.py
@@ -1,36 +1,62 @@
+from abc import ABC, abstractmethod
+from typing import List
+
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from comments.core.tools.property_reader import PropertyReader
 
 
-class WebdriverUtils(object):
-    def __init__(self, driver_name: str):
-        self._driver_name = driver_name.lower()
-        self._driver = None
-
-    def get_driver(self):
-        if self._driver is None:
-            if self._driver_name == 'chrome':
-                capabilities = DesiredCapabilities.CHROME.copy()
-            elif self._driver_name == 'firefox':
-                capabilities = DesiredCapabilities.FIREFOX.copy()
-            else:
-                raise Exception("Browser is not supported")
-            try:
-                self._driver = webdriver.Remote(PropertyReader('config.properties')
-                                                .get_browser_port(), capabilities)
-            except Exception:
-                raise Exception("Can't initialize webdriver")
-            self._driver.implicitly_wait(10)
-            self._driver.maximize_window()
-        return self._driver
-
+class Driver(ABC):
+    @abstractmethod
     def load(self, url: str):
-        self.get_driver().get(url)
+        pass
 
+    @abstractmethod
     def refresh(self):
-        self.get_driver().refresh()
+        pass
+
+    @abstractmethod
+    def find_element(self, by: By, locator: str):
+        pass
+
+    @abstractmethod
+    def find_elements(self, by: By, locator: str):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class RemoteDriver(Driver):
+    def __init__(self, driver: webdriver):
+        self._driver = driver
+        self._driver.implicitly_wait(10)
+        self._driver.maximize_window()
+
+    def load(self, url: str) -> webdriver:
+        self._driver.get(url)
+        return self
+
+    def find_element(self, by: By, locator: str) -> WebElement:
+        wait = WebDriverWait(self._driver, 5)
+        element = wait.until(EC.presence_of_element_located((by, locator)))
+        return element
+
+    def find_elements(self, by: By, locator: str) -> List[WebElement]:
+        wait = WebDriverWait(self._driver, 5)
+        elements = wait.until(
+            EC.presence_of_all_elements_located((by, locator)))
+        return elements
+
+    def refresh(self) -> webdriver:
+        self._driver.refresh()
+        return self
 
     def stop(self):
         if self._driver is not None:
@@ -39,26 +65,24 @@ class WebdriverUtils(object):
         self._driver = None
 
 
-class WebDriverFactory:
-    drivers = []
+class WebDriverFactoryFromConfig:
+    def init_driver(self):
+        browser_name = PropertyReader('config.properties').get_browser_name()
+        port = PropertyReader('config.properties').get_browser_port()
 
-    def init_driver(self, driver_name: str, port: int):
-        if driver_name.lower() == "chrome":
+        if browser_name.lower() == "chrome":
             try:
                 capabilities = DesiredCapabilities.CHROME.copy()
                 driver = webdriver.Remote(port, capabilities)
-                self.drivers.append(driver)
-                return driver
+                return RemoteDriver(driver)
             except Exception:
                 raise Exception("Can't initialize webdriver")
-        elif driver_name.lower() == "firefox":
+        elif browser_name.lower() == "firefox":
             try:
                 capabilities = DesiredCapabilities.FIREFOX.copy()
                 driver = webdriver.Remote(port, capabilities)
-                self.drivers.append(driver)
-                return driver
+                return RemoteDriver(driver)
             except Exception:
                 raise Exception("Can't initialize webdriver")
-
-    def close_drivers(self):
-        [driver.quit() for driver in self.drivers]
+        else:
+            raise Exception("Driver is not initialized.")
