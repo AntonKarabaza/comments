d9cc4b399ac9d30a3c8632aef0b6d7efe538236e
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2017-08-02 17:22:39 +0300

    Remove get methods from Comment class

--- comments/core/entities/comment.py
+++ comments/core/entities/comment.py
@@ -3,23 +3,9 @@ from typing import List
 
 
 class Comment:
-    def __init__(self, comm_id, comm_text, is_active, *comm_category: Category):
+    def __init__(self, comm_id: int, comm_text: str, is_active: bool,
+                 *comm_category: Category):
         self._id = comm_id
         self._text = comm_text
         self._is_active = is_active
         self._category = comm_category
-
-    def get_text(self) -> str:
-        return str(self._text)
-
-    def get_id(self) -> int:
-        return int(self._id)
-
-    def is_active(self) -> bool:
-        if self._is_active.lower() == 'true':
-            return True
-        else:
-            return False
-
-    def get_categories(self) -> List[Category]:
-        return list(self._category)
