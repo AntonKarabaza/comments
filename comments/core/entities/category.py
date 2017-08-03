68de7c816f63a43816b762da6d7e5cb5394cc589
AntonKarabaza <antonkarabaza@google.com>
2017-08-03 14:04:54 +0300

    Modify Category class
    Modify class to handle both integer and string representation of
    category.

--- comments/core/entities/category.py
+++ comments/core/entities/category.py
@@ -1,9 +1,10 @@
 class Category:
-    def __init__(self, category: int):
-        if category > 5 or category < 0:
-            raise Exception('Category can\'t be created')
-        else:
+    def __init__(self, category):
             self._category = category
 
     def get_category(self) -> str:
-        return 'Cat' + str(self._category)
+        try:
+            category_number = int(self._category)
+            return 'Cat' + str(category_number)
+        except ValueError:
+            return self._category
