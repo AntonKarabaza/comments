a6343e5dedeadb9c80c8852729cbea0774f13352
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2017-08-04 11:19:06 +0300

    Move all selectors to selectors.py
    All hardcoded selectors were moved to `selectors.py` to avoid code duplication and improve readability.
    
    Affects #35

--- comments/core/pages/comment_details_page.py
+++ comments/core/pages/comment_details_page.py
@@ -6,6 +6,7 @@ from comments.core.elements.textinput import TextInput
 from comments.core.pages import main_page
 from comments.core.tools.control import Control
 from comments.core.tools.webdriver_utils import Driver
+from comments.core.tools.selectors import Comment
 
 
 class CommentDetailsPage:
@@ -13,45 +14,50 @@ class CommentDetailsPage:
         self._driver = driver
 
     def click_save_button(self):
-        Button(Control(self._driver.find_element(By.CSS_SELECTOR, "#editor-navigation > input:nth-child(2)"))).click()
+        Button(Control(self._driver.find_element(By.CSS_SELECTOR,
+                                                 Comment.SAVE_BTN))).click()
         return self
 
     def click_save_and_return_button(self):
-        Button(Control(self._driver.find_element(By.CSS_SELECTOR, "#editor-navigation > input:nth-child(3)"))).click()
+        Button(Control(self._driver.find_element(By.CSS_SELECTOR,
+                                                 Comment.SAVE_RETURN_BTN))).click()
         return main_page.MainPage(self._driver)
 
     def input_comment_text(self, text: str):
-        comment_text = TextInput(Control(self._driver.find_element(By.ID, "Text")))
+        comment_text = TextInput(Control(self._driver.find_element(By.ID,
+                                                                   Comment.EDIT_TEXT)))
         comment_text.clear()
         comment_text.type(text)
         return self
 
     def edit_comment_text(self, text: str):
-        TextInput(Control(self._driver.find_element(By.ID, "Text"))).type(text)
+        TextInput(Control(self._driver.find_element(By.ID, Comment.EDIT_TEXT))).type(text)
         return self
 
     def input_comment_number(self, text: str):
-        comment_number = TextInput(Control(self._driver.find_element(By.ID, "Number")))
+        comment_number = TextInput(Control(self._driver.find_element(By.ID,
+                                                                     Comment.EDIT_NUMBER)))
         comment_number.clear()
         comment_number.type(text)
         return self
 
     def edit_comment_number(self, text: str):
-        TextInput(Control(self._driver.find_element(By.ID, "Number"))).type(text)
+        TextInput(Control(self._driver.find_element(By.ID, Comment.EDIT_NUMBER))).type(text)
         return self
 
     def check_active_status_checkbox(self):
-        active_status = CheckBox(Control(self._driver.find_element(By.CSS_SELECTOR, "#Active")))
+        active_status = CheckBox(Control(self._driver.find_element(
+            By.CSS_SELECTOR, Comment.ACTIVE)))
         if not active_status.is_checked():
             active_status.check()
         return self
 
     def uncheck_active_status_checkbox(self):
-        active_status = CheckBox(Control(self._driver.find_element(By.CSS_SELECTOR, "#Active")))
+        active_status = CheckBox(Control(self._driver.find_element(By.CSS_SELECTOR, Comment.ACTIVE)))
         if active_status.is_checked():
             active_status.check()
         return self
 
     def click_select_all_categories(self):
-        Button(Control(self._driver.find_element(By.NAME, "AllSelect"))).click()
+        Button(Control(self._driver.find_element(By.NAME, Comment.SELECT_ALL_CAT_BTN))).click()
         return self
--- comments/core/tools/selectors.py
+++ comments/core/tools/selectors.py
@@ -1,9 +1,38 @@
 class Comment:
+    ROWS = "//form[@name='commentsSelect']/table/tbody/tr"
     COMMENT_NUMBER_BY_ROW = "//form[@name='commentsSelect']/table/tbody/" \
                             "tr[{row_num}]/td[@class='numbercolumn']"
     COMMENT_TEXT_BY_ROW = "//form[@name='commentsSelect']/table/tbody/" \
-                            "tr[{row_num}]/td[@class='textcolumn']"
+                          "tr[{row_num}]/td[@class='textcolumn']"
     COMMENT_ACTIVE_BY_ROW = "//form[@name='commentsSelect']/table/tbody/" \
                             "tr[{row_num}]/td[@class='inactivecolumn']"
     COMMENT_CATEGORY_BY_ROW = "//form[@name='commentsSelect']/table/tbody/" \
-                            "tr[{row_num}]/td[@class='categorycolumn']"
+                              "tr[{row_num}]/td[@class='categorycolumn']"
+    CHECKBOX = 'td.checkedcolumn > input[type="checkbox"]'
+    EDIT_NUMBER = "Number"
+    EDIT_TEXT = "Text"
+    SAVE_BTN = "#editor-navigation > input:nth-child(2)"
+    SAVE_RETURN_BTN = "#editor-navigation > input:nth-child(3)"
+    ACTIVE = "#Active"
+    SELECT_ALL_CAT_BTN = "AllSelect"
+
+
+class Paging:
+    PAGING_LINKS = ".webgrid-footer > td > a"
+
+
+class Dialog:
+    YES_BTN = "div > button:nth-child(1)"
+    NO_BTN = "div > button:nth-child(2)"
+
+
+class MainMenu:
+    NEW_BTN = "newbutton"
+    EDIT_BTN = "input[value='Edit..']"
+    DELETE_BTN = "input[value='Delete']"
+
+
+class FilterMenu:
+    CATEGORY_FILTER = "SelectedCateg"
+    STATUS_FILTER = "SelectedStatus"
+    APPLY_BTN = "applybutton"
