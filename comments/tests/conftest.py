9cc720adc02b30b344419753b286efcd9cc4838a
AntonKarabaza <antonkarabaza@google.com>
2017-08-04 16:29:59 +0300

    Modify webdriver fixture to instantiate browser and load main page

--- comments/tests/conftest.py
+++ comments/tests/conftest.py
@@ -4,11 +4,13 @@ from comments.core.tools.webdriver_utils import WebDriverFactoryFromConfig
 
 
 @pytest.fixture(scope="module")
-def webdriver_setup(request):
+def webdriver_on_main_page(request):
     webdriver = WebDriverFactoryFromConfig().init_driver()
+    webdriver.load('http://commentssprintone.azurewebsites.net')
 
     def tear_down():
         webdriver.stop()
 
     request.addfinalizer(tear_down)
     return webdriver
+
